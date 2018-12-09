42a522ed4311dad192c1be889c544831e8d1be26
Ryan Magoon <mr.magoon5@gmail.com>
2018-12-09 00:52:10 -0500

    set up apollo for local stuff

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -5,7 +5,7 @@ import { HttpLink } from 'apollo-link-http'
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    credentials: 'same-origin',
-    uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql'
+    credentials: 'include',
+    uri: 'http://localhost:4000'
   })
 })
