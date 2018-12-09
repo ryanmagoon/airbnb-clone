f49c596f87fc3197108c480d4af2424e3d57b3f7
Ryan Magoon <mr.magoon5@gmail.com>
2018-12-09 17:03:53 -0500

    basic form mockup

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,8 +1,45 @@
-import Button from 'antd/lib/button'
+import { Button, Form, Icon, Input } from 'antd'
 import * as React from 'react'
+
+const FormItem = Form.Item
 
 export default class RegisterView extends React.PureComponent {
   render() {
-    return <Button type="primary">Button</Button>
+    return (
+      <div style={{ display: 'flex' }}>
+        <div style={{ margin: 'auto', width: 400 }}>
+          <FormItem>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />
+          </FormItem>
+          <FormItem>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />
+          </FormItem>
+          <FormItem>
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </FormItem>
+          <FormItem>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Register
+            </Button>
+          </FormItem>
+          <FormItem>
+            Or <a href="">login now!</a>
+          </FormItem>
+        </div>
+      </div>
+    )
   }
 }
