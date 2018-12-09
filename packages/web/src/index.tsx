bdbe814adc594696a3919c8929ef73da0b6ac8e4
Ryan Magoon <mr.magoon5@gmail.com>
2018-12-09 00:55:46 -0500

    wire in apollo provider

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react'
+import { ApolloProvider } from 'react-apollo'
 import * as ReactDOM from 'react-dom'
+
+import { client } from './apollo'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root') as HTMLElement
+)
 registerServiceWorker()
